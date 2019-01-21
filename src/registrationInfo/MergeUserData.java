b05e5069856f116702b0b4c0a93c86e291567396
PangQian <1835896410@qq.com>
2019-01-21 21:45:56 +0800

    新增汇总车辆注册记录部分+处理出行记录部分

--- src/registrationInfo/MergeUserData.java
+++ src/registrationInfo/MergeUserData.java
@@ -16,9 +16,60 @@ import java.util.Map;
 public class MergeUserData {
 	
 	private static String cardPath = "/home/pq/inspect/registrationInfo/card";
-	private static String vehiclePath = "H:/pangqian/vehicle";
+	private static String vehiclePath = "/home/pq/inspect/registrationInfo/vehicle";
 	
 	private static String userInfoPath = "/home/pq/inspect/cardSta.csv";
+	private static String vehicleInfoPath = "/home/pq/inspect/vehicleSta.csv";
+	
+	public static void mergeVehicleInfo(String path, String outPath) {
+
+		File file = new File(path);
+		List<String> list = Arrays.asList(file.list());
+		Map<String, LinkedList<String>> cardIDMap = new HashMap<>();
+
+		
+		for (int i = 0; i < list.size(); i++) {
+			// 依次处理每一个文件
+			String pathIn = path + "/" + list.get(i);
+			
+			try {
+				InputStreamReader inStream = new InputStreamReader(new FileInputStream(pathIn), "UTF-8");
+				BufferedReader reader = new BufferedReader(inStream);
+				
+				String line = "";
+				String[] data;
+				while ((line = reader.readLine()) != null) {
+					data = line.split(",", 8);
+					
+					//String cardId = data[0];// 用户卡编号
+					String vehId = data[0];// 车辆id
+
+					if (cardIDMap.containsKey(vehId)) {
+						LinkedList<String> listTrace = cardIDMap.get(vehId);
+						listTrace.add(line);
+						cardIDMap.put(vehId, listTrace);
+					} else {
+						LinkedList<String> listTrace = new LinkedList<>();
+						listTrace.add(line);
+						cardIDMap.put(vehId, listTrace);
+
+					}
+					
+				}
+				reader.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			System.out.println(pathIn + " read finish!");
+
+
+			}
+		
+
+		writePreExData(outPath, cardIDMap);
+	}
+	
+	
 	
 	public static void preProcess(String path, String outPath) {
 
@@ -94,7 +145,8 @@ public class MergeUserData {
 	
 	public static void main(String[] args) {
 		
-		preProcess(cardPath, userInfoPath);
+		//preProcess(cardPath, userInfoPath);
+		mergeVehicleInfo(vehiclePath, vehicleInfoPath);
 	}
 
 }
